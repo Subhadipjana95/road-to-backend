ea2884ffca19e034e3df167f398550929ebda0ce
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-05-03 21:12:21 +0530

    Update project structure and add EJS support. Changed project name to 'road-to-backend', added EJS as a dependency, and set up basic Express routes with middleware. Created initial views and stylesheets for the application.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(function(req, res, next){
-    console.log("Middleware used.");
-    next();
-})
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send("This is home pageeeee")
+    res.render('index')
 })
 
-app.get('/about', function(req, res){
-    res.send("This is about pageeeeee")
+app.get('/users/:username', function(req, res){
+    const username = req.params.username;
+    res.send(`Welcome ${username} to this website`)
 })
 
-app.listen(3000)
+app.get('/author/:authorID/:age', function(req, res){
+    const authorID = req.params.authorID;
+    const age = req.params.age;
+    res.send(`Welcome ${authorID}. Your age is ${age}`);
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
